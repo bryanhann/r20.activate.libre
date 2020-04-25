4b6dab1dff8501f9e8a2ab1b0e25674855a8dafc
Bryan Hann <nobody@nowhere>
2020-04-25 15:29:18 +0800

    Revert to dunder __map__

--- activate.sh
+++ activate.sh
@@ -1,6 +1,6 @@
 r20export R20_LIBRE $(dirname $(grealpath $0))
 r20export R20_LIBRE_incoming ${HOME}/.incoming
 r20_libre_file  () {${R20_LIBRE}/scripts/main.py $1;}
-r20_libre_raw   () {r20map r20_libre_file $(__ls__ $R20_LIBRE_incoming);}
+r20_libre_raw   () {__map__ r20_libre_file $(__ls__ $R20_LIBRE_incoming);}
 r20_libre_parse () {r20_libre_raw|sort|uniq;}
 
