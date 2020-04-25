32f22bfacf4d4acd282b5dc0be75149ea2b5c1e1
Bryan Hann <nobody@nowhere>
2020-04-25 15:24:46 +0800

    Use 'r20export' to export to environment.

--- activate.sh
+++ activate.sh
@@ -1,6 +1,6 @@
-export R20LIBRE=$(dirname $(grealpath $0))
-source ${XDG_CONFIG_HOME}/r20/libre/activate.sh
-r20_libre_file  () {${R20LIBRE}/scripts/main.py $1;}
-r20_libre_raw   () {__map__ r20_libre_file $(__ls__ $R20LIBRE_incoming);}
+r20export R20_LIBRE $(dirname $(grealpath $0))
+r20export R20_LIBRE_incoming ${HOME}/.incoming
+r20_libre_file  () {${R20_LIBRE}/scripts/main.py $1;}
+r20_libre_raw   () {r20map r20_libre_file $(__ls__ $R20_LIBRE_incoming);}
 r20_libre_parse () {r20_libre_raw|sort|uniq;}
 
