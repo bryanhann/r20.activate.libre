67c2fa239535621b2d7fb5aa72151eef14f8f203
Bryan Hann <nobody@nowhere>
2020-04-26 14:35:13 +0800

    Fix to use new R20 config variables.

--- activate.sh
+++ activate.sh
@@ -1,6 +1,4 @@
-r20export R20_LIBRE $(dirname $(grealpath $0))
-r20export R20_LIBRE_incoming ${HOME}/.incoming
-r20_libre_file  () {${R20_LIBRE}/scripts/main.py $1;}
-r20_libre_raw   () {__map__ r20_libre_file $(__ls__ $R20_LIBRE_incoming);}
+r20_libre_file  () { $(r20root libre)/scripts/main.py $1;}
+r20_libre_raw   () {__map__ r20_libre_file $(__ls__ $R20_libre_CFG_incoming);}
 r20_libre_parse () {r20_libre_raw|sort|uniq;}
 
